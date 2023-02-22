806c3d763f01c8334b79d7d91814a5d0fb553fb9
Advait <advaitdhamorikar@gmail.com>
2023-02-22 15:49:57 +0530

    Updated code with functions for ease-of-use

--- 4_cv_basics/5_masking/5_Masking_Basics.cpp
+++ 4_cv_basics/5_masking/5_Masking_Basics.cpp
@@ -2,28 +2,26 @@
 #include <iostream>
 using namespace cv;
 using namespace std;
-int main() 
+
+
+
+Mat masking(Mat image)
 {
-   Mat image;//taking an image matrix
-   image = imread("nike.png");//loading an image//
    Vec3b grey = Vec3b(230,230,230); //defining masks for the colours which you do not need in the image
    Vec3b white = Vec3b(255,255,255);
-   Vec3b black = (0,0,0);
+   
    int height = image.rows;
    int width = image.cols;
 
    Mat transparentImg = Mat::zeros(height,width, CV_8UC4); //dreates a new transparent empty image
-  
-   cout << "Height: " << height << endl;
-   cout << "Width : " << width << endl;
 
    for(int i = 0; i<height; i++)
-   {
+    {
 	for(int j = 0; j<width; j++)
 	{
 		Vec3b x = image.at<Vec3b>(i, j);//getting the pixel rgb values
 		Vec4b y  = Vec4b(x[0],x[1],x[2],255);
-		if((int(x[0])==int(grey[0]) && int(x[1])==int(grey[1]) && int(x[2])==int(grey[2])) || (int(x[0])==int(white[0]) && int(x[1])==int(white[1]) && int(x[2])==int(white[2]))) //Add the range for the colours which you want to ignore
+		if(grey==x || white==x) //Add the range for the colours which you want to ignore
 		{
 			//do not write to the transparent image if it has RGB values which we don't wish for it to have
 			; 
@@ -35,11 +33,21 @@ int main()
 	}
 	
    }
-	
-	   imwrite("true_png.png", transparentImg);
-	   imshow("center",transparentImg);
-	   waitKey(0);
-	   destroyAllWindows();
+   return transparentImg;
+}
 
+
+
+int main() 
+{
+   Mat image;//taking an image matrix
+   image = imread("mi.png");//loading an image//
+   
+   Mat new_img = masking(image);
+   imwrite("true_logo.png", new_img);
+   imshow("center",new_img);
+   waitKey(0);
+   destroyAllWindows();
+	   
    return 0;
 }
