1169d1049de40640bf2b8a62f75e82ccfeb17cfa
Mahesh Tupe <55803500+Asc91@users.noreply.github.com>
2023-03-23 16:38:55 +0530

    [Port to C++] Morphological Operations (#76)
    * added code for erosion and dilation
    
    * added code for opening, closing and gradient
    
    * added readme
    
    * removed template files
    
    * added assets folder
    
    * changed path for images
    
    * Change naming scheme
    
    * Rename main.cpp to relevenat 'algo'.cpp
    * Modify Makefile accordingly
    
    * Major changes
    1] Generalised modules for any size kernel
    2] Modified file structure
    3] Added module for padding of images
    
    * Add new image to assets folder
    
    * Add dependencies to Makefiles
    
    * Take image path as commandline argument
    
    * Remove redundant files
    
    * - Add table of contents
    - Break long paragraphs in smaller sections.
    
    * - Remove redundant function from helper.hpp
    - Add comments
    
    * Rename Assets->assets
    Rename Readme.md -> README.md
    
    * Revert Makefile changes
    
    * Change format of helper.hpp
    - made separate src and include folder
    
    * Modify Makefile
    
    * Change path of include file
    
    * Rename files
    - helper.cpp -> morphology.cpp
    - helper.hpp -> morphology.hpp
    
    * Change image path
    
    * Change helper -> morphology
    
    ---------
    
    Co-authored-by: Smit1603 <smuffinboy16@gmail.com>

---
+++ 4_cv_basics/6_morphology/5_gradient/5_gradient.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include "../include/morphology.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "usage: ./output <Image_Path>\n";
+		return -1;
+	}
+
+	// Reading the Image
+	Mat source_image = imread(argv[1], IMREAD_GRAYSCALE);
+
+	// Check if the image is created successfully or not
+	if (!source_image.data)
+	{
+		std::cout << "Could not open or find the image\n";
+		return 0;
+	}
+
+	// creating container for output image according to size and type of source image
+	Mat erod{source_image.size(), source_image.type()};
+	Mat dill{source_image.size(), source_image.type()};
+    Mat gradient{source_image.size(), source_image.type()};
+
+	// Applying erosion on source image
+	int kernel_size_erosion = 3;
+	erod = erosion(source_image, erod, kernel_size_erosion);
+
+	// Applying dilation on source image
+	int kernel_size_dilation = 3;
+	dill = dilation(source_image, dill, kernel_size_dilation);
+
+	// Taking difference of erroded and dilated image to get gradient
+	gradient = difference(dill, erod, gradient);
+
+	//Displaying both source and output image
+	namedWindow("source", WINDOW_NORMAL);
+	imshow("source", source_image);
+	
+	namedWindow("gradient", WINDOW_NORMAL);
+	imshow("gradient", gradient);
+
+	waitKey();
+
+	return 0;
+}
