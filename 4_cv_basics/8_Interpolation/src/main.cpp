467bca32d7f521a1497540e18003362ffd42010e
Raghav <raghavagarwal3050@gmail.com>
2023-03-25 21:34:22 +0530

    Added requested changes

--- 4_cv_basics/8_Interpolation/src/main.cpp
+++ 4_cv_basics/8_Interpolation/src/main.cpp
@@ -1,17 +1,15 @@
-#include "pixels_Interpolation.hpp"
+#include "interpolation.hpp"
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <cmath>
 
-using namespace Pixels;
-
 int main(int argc, char **argv)
 {
-    cv::Mat image = cv::imread("../Assets/pixel1.jpg");
-    cv::Mat upscale_image  = nearest_neighbour_interpolate(image,1000, 1000, 3);
-    cv::Mat downscale_image  =nearest_neighbour_interpolate(image , 100, 100, 3);
-    cv::imwrite("../Assets/pixeli3s.jpg",upscale_image);
-    cv:: imwrite( "../Assets/pixeli3d.jpg",downscale_image);
-    show(upscale_image , "Interpolation UpScaling!");
-    show(downscale_image , "Interpolation DownScaling!");
+    cv::Mat image = cv::imread("../assets/pixel1.jpg");                          // Reading image
+    cv::Mat downscale_image = nearest_neighbour_interpolate(image, 100, 100, 3); // Performing nearest neighbour interpolation on image
+    cv::Mat upscale_image = bilinear_interpolate(image, 1000, 1000, 3);          // Performing bilinear interpolation on image
+    cv::imwrite("../assets/pixeli3s.jpg", upscale_image);                        // Saving image
+    cv::imwrite("../assets/pixeli3d.jpg", downscale_image);                      // Saving image
+    show(upscale_image, "Interpolation UpScaling!");                             // show image using show function defined in interpolation.cpp
+    show(downscale_image, "Interpolation DownScaling!");                         // show image using show function defined in interpolation.cpp
 }
