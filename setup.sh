e8f886c77636d9838ca10ad043d53d8ae3d49770
Sanyam <118151964+SanyamGarg12@users.noreply.github.com>
2024-11-05 02:34:33 +0530

    Adding Build Tools for OS Compatibility

--- setup.sh
+++ setup.sh
@@ -12,13 +12,13 @@ else
     if grep -qi "debian" /etc/os-release; then
         echo "Detected Debian-based system"
         sudo apt-get update -y
-        sudo apt-get install libopencv-dev libsdl2-2.0-0 libsdl2-image-dev libsdl2-dev -y
+        sudo apt-get install build-essential libopencv-dev libsdl2-2.0-0 libsdl2-image-dev libsdl2-dev -y
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "Detected macOS"
         brew install opencv sdl2
     elif grep -qi "arch" /etc/os-release; then
         echo "Detected Arch Linux"
-        sudo pacman -Sy opencv hdf5 glew vtk fmt sdl2
+        sudo pacman -Sy base-devel opencv hdf5 glew vtk fmt sdl2
     else
         echo "Unknown OS, cannot install OpenCV"
         exit 1
