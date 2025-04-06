2afd9eac8f1269ea978fd60774ebee2ed09da7ad
JaytidaA <ajoil4068@gmail.com>
2025-04-06 08:15:08 +0530

    Changed include directive in 4_cv_basics/5_masking

--- 4_cv_basics/5_masking/main.cpp
+++ 4_cv_basics/5_masking/main.cpp
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-#include <opencv4/opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
 #include <iostream>
 using namespace std;
 
